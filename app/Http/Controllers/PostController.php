ca500b1183659850283ed50c71dc51c78b768c6d
saepulfariz <saepulfariz108@gmail.com>
2026-09-12 07:15:00 +0700

    feat: add post creation functionality with PostController and Create page

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -22,4 +22,39 @@ class PostController extends Controller
             'posts' => $posts
         ]);
     }
+
+
+    /**
+     * create
+     *
+     * @return void
+     */
+    public function create()
+    {
+        return inertia('Posts/Create');
+    }
+
+    /**
+     * store
+     *
+     * @param  mixed $request
+     * @return void
+     */
+    public function store(Request $request)
+    {
+        //set validation
+        $request->validate([
+            'title'   => 'required',
+            'content' => 'required',
+        ]);
+
+        //create post
+        Post::create([
+            'title'     => $request->title,
+            'content'   => $request->content
+        ]);
+
+        //redirect
+        return redirect()->route('posts.index')->with('message', 'Data Berhasil Disimpan!');
+    }
 }
